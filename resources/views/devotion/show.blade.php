0c4afccd6a252c416e3e992f9d1139cff7f92cc4
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-03-18 20:46:48 +0000

    scrapped

--- resources/views/devotion/show.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>{{ $devotion->title }}</h1>
-    <p><strong>Date:</strong> {{ $devotion->date->format('F j, Y') }}</p>
-    <p>{{ $devotion->content }}</p>
-    <a href="{{ url('/devotion') }}">Back to Devotions</a>
-@endsection
